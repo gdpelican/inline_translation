e8d22aa8e3b12093d95be4f73eb5d3b38505be80
James Kiesel <james.kiesel@gmail.com>
2015-08-11 13:48:27 -0700

    Rename tests, remove worthless test

--- test/lib/babbel_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class InlineTranslationTest < UnitTest
-  describe "InlineTranslation version" do
-    it "return the correct version" do
-      InlineTranslation::VERSION.must_equal '0.0.1'
-    end
-  end
-end
